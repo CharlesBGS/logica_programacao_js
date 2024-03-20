91314811aa7bf69ec7222895e84a2e3dc5a66298
Charles Belchior <charlesbelchiorgs@gmail.com>
2024-03-20 17:39:17 -0300

    Concusão.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 alert('Boas-vindas ao jogo do Número Secreto!');
-let numeroSecreto = 9;
+let dificuldade = 100;
+let numeroSecreto = parseInt(Math.random() * dificuldade + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
@@ -9,14 +10,15 @@ let tentativas = 1;
 // console.log(`Resultado da comparação: `, chute == numeroSecreto);
 
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 01 e 30');
+    chute = prompt(`Escolha um número entre 01 e ${dificuldade}`);
 
     // Adicione um console.log para verificar o valor de "chute" após a entrada do usuário
     // console.log(`Valor do chute: `, chute);
 
     // se chute for igual ao numeroSecreto
     if (chute == numeroSecreto) {
-        alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+        break;
+
     } else {
         // Adicione um console.log para verificar o valor de "numeroSecreto" quando o jogador erra
         // console.log(`Valor do numero secreto: `, numeroSecreto);
@@ -29,3 +31,14 @@ while (chute != numeroSecreto) {
     // alert('Você Errou! :(');
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
+
+/*
+if (tentativas > 1) {
+    alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+} else {
+    alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa!`);
+}
+*/
